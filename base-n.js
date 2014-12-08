b97a4c1baef2f0b16e721744bb3422ab2a3f6e88
Julia <julia@jfr.fritz.box>
2014-12-08 15:42:27 +0100

    conversion of any n-based number system to decimal an all new and shinier way, with a much better file name. just kidding. AND error handling. for now, converts from any position based number system to decimal.

---
+++ base-n.js
@@ -0,0 +1,28 @@
+function convertToDec (sourceNumber, sourceCharacters) {
+    var result = 0;
+    var sourceBase = sourceCharacters.length;
+    for (var i=0; i < sourceNumber.length; i++) {
+        var currentDigit = sourceNumber[i];
+        var position = sourceNumber.length - i - 1;
+        var characterValue = sourceCharacters.indexOf(currentDigit);
+        if (characterValue === -1) {
+            throw new Error(currentDigit + ' is not contained within the character set: ' + sourceCharacters);
+        }  
+        result += Math.pow(sourceBase, position) * characterValue;
+    }
+    return result;
+}
+
+// we assume for this function 3 string arguments, a character set for the
+// source system, a character set for the target system, and the number in the
+// source system (as a string)
+function baseToBase (sourceCharacters, targetCharacters, sourceNumber) {
+    var sourceBase = sourceCharacters.length;
+    var targetBase = targetCharacters.length;
+
+    // replace every position in both string bases with their index position
+    // number/value/whatever in decimals take the current number and convert
+    // it to decimal, by starting at
+    // position 0
+}
+
