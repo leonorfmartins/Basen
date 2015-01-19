975054d1438ca5033843a05ec5bd75df5a997bea
Leonor Oliveira <leonorfmartins@gmail.com>
2015-01-19 14:01:42 +0100

    first implementation of convertFromDec(). Tests are missing

--- base-n.js
+++ base-n.js
@@ -1,6 +1,6 @@
 // This function will take a string containing the number to be converted and another string containing the character set of the number system it is converting from.
 
-function convertToDec (sourceNumber, sourceCharacters) {
+function convertToDec(sourceNumber, sourceCharacters) {
     var result = 0;
     var sourceBase = sourceCharacters.length;
     for (var i=0; i < sourceNumber.length; i++) {
@@ -17,6 +17,62 @@ function convertToDec (sourceNumber, sourceCharacters) {
     return result;
 }
 
+/*
+* 1. check if decNumber < TargetBase
+* 1.1. if yes, return decNumber
+* 2. result = DecNumber / TargetBase
+* 3. check if result < TargetBase
+* 4. if not, goto 1 and increase psoition.
+* 5. if yes, use the result as digit at position
+ */   
+
+function convertFromDec(decNumber, targetCharacters) {
+    var targetBase = targetCharacters.length;
+    if (decNumber < targetBase) {
+        // fast path for trivial case: early return  
+        return decNumber;
+    } 
+    var digit;
+    var result = [];
+    var remaining = decNumber;
+    //keep producing digits until the sum of their values
+    //equals the decimal input.
+    while (remaining > 0) {
+        var x = remaining;
+        var position = 0;
+        //find the most significant digit of the remaining value
+        do { 
+            if (x >= targetBase) {
+                digit = Math.floor(x / targetBase);
+                console.log("digit", digit);
+                position++;
+            } else {
+                digit = x;
+                console.log("digit", digit);
+            }
+            if (digit < targetBase) {
+                result[position] = digit;
+                remaining -= Math.pow(targetBase, position) * digit;
+                console.log("remaining", remaining);
+            } else {
+                console.log("position", position);
+                x = digit;
+            }
+        } while (digit >= targetBase);
+    }
+    result = result.reverse();
+    for (var i = 0; i < result.length; i++) {
+        if (result[i] === undefined) {
+            result[i] = targetCharacters[0];
+        } else {
+            result[i] = targetCharacters[result[i]];
+        }
+    }
+    return result;
+}
+
+
+
 // we assume for this function 3 string arguments, a character set for the
 // source system, a character set for the target system, and the number in the
 // source system (as a string)
@@ -29,7 +85,6 @@ function baseToBase (sourceCharacters, targetCharacters, sourceNumber) {
     // it to decimal, by starting at
     // position 0
 }
-
 module.exports = {
     convertToDec: convertToDec
 };
