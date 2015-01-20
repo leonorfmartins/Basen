5fb9fb8f3323d38658ae6ddd3f4531a748edcf71
jfr3000 <julia.friesel@gmail.com>
2015-01-20 18:44:37 +0100

    Now with any-to-any conversion, added tests and presets for popular number systems.

--- base-n.js
+++ base-n.js
@@ -1,5 +1,8 @@
+var fs = require('fs');
+var bases = JSON.parse(fs.readFileSync('./bases.json'));
+
 // This function will take a string containing the number to be converted and another string containing the character set of the number system it is converting from.  
- function convertToDec(sourceNumber, sourceCharacters) {
+function convertToDec(sourceNumber, sourceCharacters) {
     var result = 0;
     var sourceBase = sourceCharacters.length;
     for (var i=0; i < sourceNumber.length; i++) {
@@ -71,16 +74,13 @@ function convertFromDec(decNumber, targetCharacters) {
 // we assume for this function 3 string arguments, a character set for the
 // source system, a character set for the target system, and the number in the
 // source system (as a string)
-function baseToBase (sourceCharacters, targetCharacters, sourceNumber) {
-    var sourceBase = sourceCharacters.length;
-    var targetBase = targetCharacters.length;
-
-    // replace every position in both string bases with their index position
-    // number/value/whatever in decimals take the current number and convert
-    // it to decimal, by starting at
-    // position 0
+function baseToBase(sourceCharacters, targetCharacters, sourceNumber) {
+    var decNumber = convertToDec(sourceNumber, sourceCharacters);
+    return convertFromDec(decNumber, targetCharacters);
 }
 module.exports = {
     convertToDec: convertToDec,
-    convertFromDec: convertFromDec
+    convertFromDec: convertFromDec,
+    baseToBase: baseToBase,
+    BASES: bases
 };
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+var bases = require('./bases.json');
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
 var convert = require('./base-n');
@@ -13,8 +14,31 @@ lab.test('convertFromDec should work for any base', function(done) {
     expect(convert.convertFromDec(0xFFF, '01')).toEqual('111111111111');
     expect(convert.convertFromDec(0xF1, '01')).toEqual('11110001');
     expect(convert.convertFromDec(6, 'fh^')).toEqual('^f');
-    expect(convert.convertFromDec(255, '0123456789abcdef')).toEqual('ff');
+    expect(convert.convertFromDec(255, convert.BASES.hex)).toEqual('ff');
     expect(convert.convertFromDec(064, '01234567')).toEqual('64');
     expect(convert.convertFromDec(0xf43cde678ad4, '0123456789abcdef')).toEqual('f43cde678ad4');
+    done();
+});
+
+lab.test('baseToBase should remove leading zeros', function(done) {
+    expect(convert.baseToBase('01', '01', '0001000')).toEqual('1000');
+    done();
+});
+
+lab.test('baseToBase should have no effect when source and target characters are identical', function(done) {
+    expect(convert.baseToBase('01234567890abcdef', '01234567890abcdef', '1234fdce34ff8')).toEqual('1234fdce34ff8');
+    expect(convert.baseToBase('01', '01', '10001000')).toEqual('10001000');
+    done();
+}); 
+
+lab.test('baseToBase should correctly convert from one n-based system to another', function(done) {
+    expect(convert.baseToBase('01', '0123456789', '10001000')).toEqual('136');
+    done();
+});
+
+
+lab.test("baseToBase should not blow up when confronted with a long base 64 number", function(done) {
+    var result = convert.baseToBase(bases.b64, '0123456789', 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCzS9yLPIyUZ0ZrItW8YjeMNC3kYcw82KeMo4jCgTsagWtktbtum0FvaNp0hQ5b0FRha9FLtWN77PxoeIfr9VBQWgqQoXjyQI+z/QSCAiByWekk4Qm8g9eWM+cv7yEOgIvVpli69h/YwECe9MGbrU6vmfSjIpUeyUM84v8K86gimWb2pHSMWvWR6mFk3qo0JZu+JV6wKhfNLyzJXwVIhC7+yd6eUuRyRG9NATnbvUxHxO37XMGn+jVZXxKvsGpy8Y4g1es8G9noZBm+Mn4Wd22RdKfAUrYWS12nQfGJf4aJFxvVl/4y+olFM+HhAcZ6wAQn6p4bGqq5VfbK31QpnKj1');
+    console.log(result);
     done();
 });
